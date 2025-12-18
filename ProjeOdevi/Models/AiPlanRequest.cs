afa2b7323bb14e39358dd03b551e329c62e598a1
CaniReese <mesutcan63788226@gmail.com>
2025-12-18 23:36:27 +0300

    Add AI plan request model

---
+++ ProjeOdevi/Models/AiPlanRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjeOdevi.Models
+{
+    public class AiPlanRequest
+    {
+        public int Age { get; set; }
+        public int Height { get; set; } // cm
+        public int Weight { get; set; } // kg
+        public string Goal { get; set; }
+    }
+}
